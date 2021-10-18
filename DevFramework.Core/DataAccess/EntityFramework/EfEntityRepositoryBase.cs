006faa29a15ad671f3dfe0c7d955b836a5e01a32
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-10-18 16:25:36 +0300

    Entity Framework implementasyonunun gerçekleştirilmesi

--- DevFramework.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ DevFramework.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -15,27 +15,52 @@ namespace DevFramework.Core.DataAccess.EntityFramework
     {
         public TEntity Add(TEntity entity)
         {
-            throw new NotImplementedException();
+            using (var context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+                return entity;
+            }
         }
 
         public void Delete(TEntity entity)
         {
-            throw new NotImplementedException();
+            using (var context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
         }
 
         public TEntity Get(Expression<Func<TEntity, bool>> filter)
         {
-            throw new NotImplementedException();
+            using (var context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
         }
 
         public List<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
         {
-            throw new NotImplementedException();
+            using (var context = new TContext())
+            {
+                return filter == null
+                    ? context.Set<TEntity>().ToList()
+                    : context.Set<TEntity>().Where(filter).ToList();
+            }
         }
 
         public TEntity Update(TEntity entity)
         {
-            throw new NotImplementedException();
+            using (var context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+                return entity;
+            }
         }
     }
 }
