90ae76c673579b236b403a6ce2506f31960df8c7
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-10-22 12:55:17 +0300

    Business Module implementasyonu

---
+++ DevFramework.Northwind.Business/DependencyResolvers/Ninject/BusinessModule.cs
@@ -0,0 +1,22 @@
+﻿using DevFramework.Northwind.Business.Abstract;
+using DevFramework.Northwind.Business.Concrete.Managers;
+using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.DataAccess.Concrete.EntityFramework;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.Business.DependencyResolvers.Ninject
+{
+    public class BusinessModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IProductService>().To<ProductManager>().InSingletonScope();
+            Bind<IProductDal>().To<EfProductDal>().InSingletonScope();
+        }
+    }
+}
