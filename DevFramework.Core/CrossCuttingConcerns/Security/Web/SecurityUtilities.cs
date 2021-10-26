033452a521c439606fc328cd4ffd3f1c56f3ce58
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-10-27 00:01:33 +0300

    FormsAuthTicket'tan Identity için metotlar dolduruldu.

--- DevFramework.Core/CrossCuttingConcerns/Security/Web/SecurityUtilities.cs
+++ DevFramework.Core/CrossCuttingConcerns/Security/Web/SecurityUtilities.cs
@@ -19,50 +19,56 @@ namespace DevFramework.Core.CrossCuttingConcerns.Security.Web
                 Roles = SetRoles(ticket),
                 FirstName = SetFirstName(ticket),
                 LastName = SetLastName(ticket),
-                AuthenticationType = SetAuthType(ticket),
-                IsAuthenticated = SetIsAuthenticated(ticket)
+                AuthenticationType = SetAuthType(),
+                IsAuthenticated = SetIsAuthenticated()
             };
             return identity;
         }
 
-        private bool SetIsAuthenticated(FormsAuthenticationTicket ticket)
+        private bool SetIsAuthenticated()
         {
-            throw new NotImplementedException();
+            return true;
         }
 
-        private string SetAuthType(FormsAuthenticationTicket ticket)
+        private string SetAuthType()
         {
-            throw new NotImplementedException();
+            return "Forms";
         }
 
         private string SetLastName(FormsAuthenticationTicket ticket)
         {
-            throw new NotImplementedException();
+            string[] data = ticket.UserData.Split('|');
+            return data[3];
         }
 
         private string SetFirstName(FormsAuthenticationTicket ticket)
         {
-            throw new NotImplementedException();
+            string[] data = ticket.UserData.Split('|');
+            return data[2];
         }
 
         private string SetEmail(FormsAuthenticationTicket ticket)
         {
-            throw new NotImplementedException();
+            string[] data = ticket.UserData.Split('|');
+            return data[0];
         }
 
         private string[] SetRoles(FormsAuthenticationTicket ticket)
         {
-            throw new NotImplementedException();
+            string[] data = ticket.UserData.Split('|');
+            string[] roles = data[1].Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            return roles;
         }
 
         private string SetName(FormsAuthenticationTicket ticket)
         {
-            throw new NotImplementedException();
+            return ticket.Name;
         }
 
         private Guid SetId(FormsAuthenticationTicket ticket)
         {
-            throw new NotImplementedException();
+            string[] data = ticket.UserData.Split('|');
+            return new Guid(data[4]);
         }
     }
 }
