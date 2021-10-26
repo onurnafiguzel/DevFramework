ec2632cda4dfbfbcab52cfb229dbf9ed5f326e17
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-10-26 22:34:33 +0300

    FormsAuthTicket'tan Identity için boş methodlar tanımlandı

---
+++ DevFramework.Core/CrossCuttingConcerns/Security/Web/SecurityUtilities.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Security;
+
+namespace DevFramework.Core.CrossCuttingConcerns.Security.Web
+{
+    public class SecurityUtilities
+    {
+        public Identity FormsAuthTicketToIdentity(FormsAuthenticationTicket ticket)
+        {
+            var identity = new Identity
+            {
+                Id = SetId(ticket),
+                Name = SetName(ticket),
+                Email = SetEmail(ticket),
+                Roles = SetRoles(ticket),
+                FirstName = SetFirstName(ticket),
+                LastName = SetLastName(ticket),
+                AuthenticationType = SetAuthType(ticket),
+                IsAuthenticated = SetIsAuthenticated(ticket)
+            };
+            return identity;
+        }
+
+        private bool SetIsAuthenticated(FormsAuthenticationTicket ticket)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string SetAuthType(FormsAuthenticationTicket ticket)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string SetLastName(FormsAuthenticationTicket ticket)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string SetFirstName(FormsAuthenticationTicket ticket)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string SetEmail(FormsAuthenticationTicket ticket)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string[] SetRoles(FormsAuthenticationTicket ticket)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string SetName(FormsAuthenticationTicket ticket)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Guid SetId(FormsAuthenticationTicket ticket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
