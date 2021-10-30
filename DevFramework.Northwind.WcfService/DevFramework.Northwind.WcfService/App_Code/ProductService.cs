3de7bb86e99ec8f510a3ba6613ca41522b77f213
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-10-31 00:40:18 +0300

    Git Pull proje yeni branch ile buradan devam edecek

---
+++ DevFramework.Northwind.WcfService/DevFramework.Northwind.WcfService/App_Code/ProductService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DevFramework.Northwind.Business.Abstract;
+
+/// <summary>
+/// Summary description for ProductService
+/// </summary>
+public class ProductService : IProductService
+{
+    public ProductService()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
